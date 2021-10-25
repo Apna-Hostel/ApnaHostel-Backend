515e50191d1b9fd41286419fd132cae72722e477
AnshumanThakur2799 <anshuman2799@gmail.com>
2021-10-25 12:10:38 +0530

    adding individual ids in student router and some bug fixes

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -6,6 +6,7 @@ const Students = require('../models/students');
 const User = require('../models/users');
 var authenticate = require('../authenticate');
 const cors = require('./cors');
+const { response } = require('../app');
 
 studentRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
@@ -52,3 +53,55 @@ studentRouter.route('/')
             .catch((err) => next(err))
     })
 
+studentRouter.route('/:studetnId')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+    .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+        Students.findById(req.params.studetnId)
+            .then((student) => {
+                if (student != null) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-type', 'application/json');
+                    res.json(student);
+                } else {
+                    const err = new Error("student not found");
+                    err.status = 403;
+                    return (next(err));
+                }
+            }, (err) => next(err))
+            .catch(err => next(err));
+    })
+
+    .put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Students.findById(req.params.studentId)
+            .then((student) => {
+                if (student != null) {
+                    console.log('hello');
+                    Students.findByIdAndUpdate(req.params.studentId, {
+                        $set: req.body
+                    }, { new: true })
+                        .then((newStudent) => {
+                            Students.findById(newStudent._id)
+                                .then((stu) => {
+                                    res.statusCode = 200;
+                                    res.setHeader('Content-type', 'application/json');
+                                    res.json(stu);
+                                }, err => next(err))
+                        }, err => next(err))
+                }
+            }, err => next(err))
+    })
+
+    .post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.end('post operations not available')
+    })
+
+    .delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Students.findByIdAndDelete(req.params.studetnId)
+            .then((response) => {
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'application/json');
+                res.json(response);
+            }, err => next(err))
+    })
+
+module.exports = studentRouter;
